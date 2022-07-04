3c6515fa7dab7e615461a313da8da16a10b55027
Lucy Ironmonger <37961811+lucy-ironmonger@users.noreply.github.com>
2022-07-04 23:00:47 +0100

    CSS Modules and media queries

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -23,9 +23,7 @@ const Button = styled.button`
   background: #ac0e77;
   border-color: #ac0e77;
   box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-}
-`
-
+}`
 
 // const Button = props => {
 //   return (
